dc53c3083c7c57a7a5be8011334dd30ae464b5ae
PhamPhuc0903 <Pphuc8554@gmail.com>
2026-08-24 00:16:57 +0700

    fix(api): Fix code for pass CI

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -20,7 +20,7 @@ export default tseslint.config(
     },
     rules: {
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/explicit-function-return-types': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
     },
   },
   ...tseslint.configs.recommended,
